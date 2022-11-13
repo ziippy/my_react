5d7426f48caf75b643f2be06aaf969c1516662f1
ziippy <ziippy@naver.com>
2022-11-13 20:14:23 +0900

    Link 까지

--- 06_codethat/src/Main.js
+++ 06_codethat/src/Main.js
@@ -1,15 +1,40 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./components/App";
 import HomePage from "./pages/HomePage";
+import CourseListPage from "./pages/CourseListPage";
+import CoursePage from "./pages/CoursePage";
+import WishListPage from "./pages/WishlistPage";
+import QuestionListPage from "./pages/QuestionListPage";
+import QuestionPage from "./pages/QuestionPage";
 
 function Main() {
     return (
         <BrowserRouter>
             <App>
-                <HomePage />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="courses" element={<CourseListPage />} />
+                    <Route
+                        path="courses/react-frontend-develpment"
+                        element={<CoursePage />}
+                    />
+                    <Route path="questions" element={<QuestionListPage />} />
+                    <Route path="questions/616825" element={<QuestionPage />} />
+                    <Route path="wishlist" element={<WishListPage />} />
+                </Routes>
             </App>
         </BrowserRouter>
     );
 }
+
+// function Main() {
+//     return (
+//         <BrowserRouter>
+//             <App>
+//                 <HomePage />
+//             </App>
+//         </BrowserRouter>
+//     );
+// }
 
 export default Main;
--- 06_codethat/src/components/Nav.js
+++ 06_codethat/src/components/Nav.js
@@ -1,23 +1,47 @@
-import Container from './Container';
-import UserMenu from './UserMenu';
-import logoImg from '../assets/logo.svg';
-import styles from './Nav.module.css';
+import Container from "./Container";
+import UserMenu from "./UserMenu";
+import logoImg from "../assets/logo.svg";
+import styles from "./Nav.module.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
-  return (
-    <div className={styles.nav}>
-      <Container className={styles.container}>
-        <img src={logoImg} alt="Codethat Logo" />
-        <ul className={styles.menu}>
-          <li>카탈로그</li>
-          <li>커뮤니티</li>
-          <li>
-            <UserMenu />
-          </li>
-        </ul>
-      </Container>
-    </div>
-  );
+    return (
+        <div className={styles.nav}>
+            <Container className={styles.container}>
+                <Link to="/">
+                    <img src={logoImg} alt="Codethat Logo" />
+                </Link>
+                <ul className={styles.menu}>
+                    <li>
+                        <Link to="/courses">카탈로그</Link>
+                    </li>
+                    <li>
+                        <Link to="/questions">커뮤니티</Link>
+                    </li>
+                    <li>
+                        <UserMenu />
+                    </li>
+                </ul>
+            </Container>
+        </div>
+    );
 }
+
+// function Nav() {
+//   return (
+//     <div className={styles.nav}>
+//       <Container className={styles.container}>
+//         <img src={logoImg} alt="Codethat Logo" />
+//         <ul className={styles.menu}>
+//           <li>카탈로그</li>
+//           <li>커뮤니티</li>
+//           <li>
+//             <UserMenu />
+//           </li>
+//         </ul>
+//       </Container>
+//     </div>
+//   );
+// }
 
 export default Nav;
